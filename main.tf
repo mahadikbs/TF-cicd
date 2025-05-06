e5b5e48941e88f37249a7443a30cf7b1edc38794
Brahmadev Shivaji Mahadik <brahmadev.mahadik@yash.com>
2025-05-06 08:24:37 +0530

    update actions workflow

--- main.tf
+++ main.tf
@@ -1,9 +1,6 @@
 resource "aws_instance" "terraform-test" {
     ami = var.ami-id
     instance_type = var.ec2-type
-    tags = {
-        name = terraform-test
-    }  
 }
 
 resource "aws_s3_bucket" "terrform-test-brahma" {
